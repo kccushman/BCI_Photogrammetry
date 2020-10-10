6006d357babc75680aabacc21ab972aebfa1d147
KC Cushman <cushman.kc@gmail.com>
2020-10-10 12:57:23 -0400

    -- Add 2017 to 2015 -- Add retile 2018 -- Add retile 2019 -- Add troubleshooting options

--- MakeHtChangeRaster.R
+++ MakeHtChangeRaster.R
@@ -3,172 +3,193 @@ gridInfo <- read.csv("gridInfo.csv")
 
 #### 2009 to 2015 #### 
 
-
 # Remove overlap from tiles
 
-  #overlap <- 25
+#overlap <- 30
+
+path1 <- "D:/BCI_Spatial/Lidar_Data/"
+path2 <- "D:/BCI_Spatial/UAV_Data/TiledPointClouds/"
+
+# 2009
+for(i in 1:dim(gridInfo)[1]){
   
-  path1 <- "D:/BCI_Spatial/Lidar_Data/"
-  path2 <- "D:/BCI_Spatial/UAV_Data/TiledPointClouds/"
+  data <- lidR::clip_rectangle(las = lidR::readLAS(paste0(path1,"BCI09Tiles/BCI09_",gridInfo$ID[i],".laz")),
+                               xleft=gridInfo$xmin[i],
+                               xright=gridInfo$xmax[i],
+                               ybottom=gridInfo$ymin[i],
+                               ytop=gridInfo$ymax[i])
+  lidR::writeLAS(las = data,
+                 file = paste0(path1,"BCI09Tiles_trim/BCI09at_",gridInfo$ID[i],".laz"))
+}
+
+# 2015
+for(i in 1:dim(gridInfo)[1]){
   
-  # 2009
-    for(i in 1:dim(gridInfo)[1]){
-     
-      data <- lidR::clip_rectangle(las = lidR::readLAS(paste0(path1,"BCI09Tiles/BCI09_",gridInfo$ID[i],".laz")),
-                                   xleft=gridInfo$xmin[i],
-                                   xright=gridInfo$xmax[i],
-                                   ybottom=gridInfo$ymin[i],
-                                   ytop=gridInfo$ymax[i])
-      lidR::writeLAS(las = data,
-                     file = paste0(path1,"BCI09Tiles_trim/BCI09at_",gridInfo$ID[i],".laz"))
-    }
-  
-  # 2015
-    for(i in 1:dim(gridInfo)[1]){
-      
-      data <- lidR::clip_rectangle(las = lidR::readLAS(paste0(path2,"BCI15Tiles_aligned/BCI15a_",gridInfo$ID[i],".las")),
-                                   xleft=gridInfo$xmin[i],
-                                   xright=gridInfo$xmax[i],
-                                   ybottom=gridInfo$ymin[i],
-                                   ytop=gridInfo$ymax[i])
-      if(length(data@data$X)>0){
-        lidR::writeLAS(las = data,
-                       file = paste0(path2, "BCI15Tiles_alignedTrim/BCI15at_",gridInfo$ID[i],".laz"))
-        }
-    }
+  data <- lidR::clip_rectangle(las = lidR::readLAS(paste0(path2,"BCI15Tiles_aligned/BCI15a_",gridInfo$ID[i],".las")),
+                               xleft=gridInfo$xmin[i],
+                               xright=gridInfo$xmax[i],
+                               ybottom=gridInfo$ymin[i],
+                               ytop=gridInfo$ymax[i])
+  if(length(data@data$X)>0){
+    lidR::writeLAS(las = data,
+                   file = paste0(path2, "BCI15Tiles_alignedTrim/BCI15at_",gridInfo$ID[i],".laz"))
+  }
+}
 
 
 # Make canopy height rasters for each year
 
-  cat09at <- lidR::catalog(paste0(path1,"BCI09Tiles_trim/"))
-  cat15at <- lidR::catalog(paste0(path2,"BCI15Tiles_alignedTrim/"))
-  
-  chm09 <- lidR::grid_canopy(cat09at,
-                             res = 2,
-                             algorithm = lidR::p2r(subcircle=1))
-  
-  chm15 <- lidR::grid_canopy(cat15at,
-                             res = 1,
-                             algorithm = lidR::p2r(subcircle=0.5))
-  
+cat09at <- lidR::catalog(paste0(path1,"lidar tiles 2009/lidar tiles 2009/"))
+cat15at <- lidR::catalog(paste0(path2,"BCI15Tiles_alignedTrim/"))
+
+chm09 <- lidR::grid_canopy(cat09at,
+                           res = 1,
+                           algorithm = lidR::p2r(subcircle=0.5))
+
+chm15 <- lidR::grid_canopy(cat15at,
+                           res = 1,
+                           algorithm = lidR::p2r(subcircle=0.5))
+
 # Plot canopy height rasters and canopy height change
-  colBrks1 <- seq(1,250)
-  raster::plot(chm09,
-               main="Canopy height 2009",
-               col = terrain.colors(length(colBrks1), rev=T),
-               breaks = colBrks1)
-  raster::plot(chm15,
-               main="Canopy height 2015",
-               col = terrain.colors(length(colBrks1), rev=T),
-               breaks = colBrks1)
+colBrks1 <- seq(1,250)
+raster::plot(chm09,
+             main="Canopy height 2009",
+             col = terrain.colors(length(colBrks1), rev=T),
+             breaks = colBrks1)
+raster::plot(chm15,
+             main="Canopy height 2015",
+             col = terrain.colors(length(colBrks1), rev=T),
+             breaks = colBrks1)
+chm09 <- raster::crop(chm09, raster::extent(chm15))
 
-dHeight17to18 <- chm18-chm17
-
-colBrks2 <- c(-100,-20,-10,-5,-1,-0.5,0.5,1,5,10,20,100)
-colPal2 <- colorRampPalette(c("red","darksalmon","yellow",
-                              "white",
-                              "aliceblue","cornflowerblue","darkblue"))
-
-
-raster::plot(dHeight17to18,
-             col = colPal2(length(colBrks2)-1),
-             breaks = colBrks2,
-             main = "Corrected 2017 to 2018 height change")
-raster::plot(dHeight18to19,
-             col = colPal2(length(colBrks2)-1),
-             breaks = colBrks2,
-             main = "Corrected 2018 to 2019 height change")
-
-raster::writeRaster(chm17, file = "CHM_2017_corrected.tif")
-raster::writeRaster(chm18, file = "CHM_2018_corrected.tif")
-raster::writeRaster(chm19, file = "CHM_2019_corrected.tif")
-
-chm17 <- raster::raster("CHM_2017_corrected.tif")
-chm18 <- raster::raster("CHM_2018_corrected.tif")
-chm19 <- raster::raster("CHM_2019_corrected.tif")
-
-# Compare to uncorrected rasters
-cat17_old <- lidR::catalog("2017_06_22_AllBCI_EBEE.las")
-cat18_old <- lidR::catalog("2018_Todos_BCI_Dipteryx_EBEE_PointCloud.las")
-cat19_old <- lidR::catalog("Cloudpoint_2019_06_24_BCI_Dipteyx.las")
-
-chm17_old <- lidR::grid_canopy(cat17_old,
-                           res = 1,
-                           algorithm = lidR::p2r(subcircle=0.1))
-
-chm18_old <- lidR::grid_canopy(cat18_old,
-                           res = 1,
-                           algorithm = lidR::p2r(subcircle=0.1))
-
-chm19_old <- lidR::grid_canopy(cat19_old,
-                           res = 1,
-                           algorithm = lidR::p2r(subcircle=0.1))
-
-chm17_old <- raster::crop(chm17_old[[1]],raster::extent(chm17))
-chm18_old <- raster::crop(chm18_old[[1]],raster::extent(chm17))
-chm19_old <- raster::crop(chm19_old[[1]],raster::extent(chm17))
-
-dHeight17to18_old <- chm18_old-chm17_old
-dHeight18to19_old <- chm19_old-chm18_old
-
-colBrks2 <- c(-100,-20,-10,-5,-1,-0.5,0.5,1,5,10,20,100)
-colPal2 <- colorRampPalette(c("red","darksalmon","yellow",
-                              "white",
-                              "aliceblue","cornflowerblue","darkblue"))
-raster::plot(dHeight17to18_old,
-             col = colPal2(length(colBrks2)-1),
-             breaks = colBrks2,
-             main = "Uncorrected 2017 to 2018 height change")
-raster::plot(dHeight18to19_old,
-             col = colPal2(length(colBrks2)-1),
-             breaks = colBrks2,
-             main = "Uncorrected 2018 to 2019 height change")
-
-## New alignment for 2018-2019
-mask <- raster::raster("BCI_mask/BCI_mask.tif")
-mask <- raster::crop(mask,raster::extent(chm19))
-
-
-chm18_old <- raster::crop(chm18_old[[1]],raster::extent(chm19))
-
-
-dHeight18to19 <- chm19-chm18_old
-dHeight18to19[mask==0] <- NA
-
+dHeight09to15 <- chm15-chm09
 
 colBrks2 <- c(-100,-20,-10,-5,-1,-0.5,0.5,1,5,10,20,100)
 colPal2 <- colorRampPalette(c("red","darksalmon","yellow",
                               "white",
                               "aliceblue","cornflowerblue","darkblue"))
 
-pdf(height=10, width=8, file="canopyHtchange.pdf")
-raster::plot(dHeight18to19,
+raster::plot(dHeight09to15,
              col = colPal2(length(colBrks2)-1),
              breaks = colBrks2,
-             main = "Corrected 2018 to 2019 height change")
-abline(v=gridInfo$xmin)
-abline(h=gridInfo$ymin)
-dev.off()
-
-raster::writeRaster(chm19, file = "CHM_2019_corrected_2.tif")
-raster::writeRaster(chm18_old, file = "CHM_2018_uncorrected.tif")
+             main = "Corrected 2009 to 2015 height change")
 
 
-## try something out
-gridInfo$QC <- NA
+raster::writeRaster(chm09, file = "CHM_2009.tif")
+raster::writeRaster(chm15, file = "CHM_2015_corrected.tif")
 
+#### 2015 to 2017 #### 
+
+# Remove overlap from tiles
+
+#overlap <- 30
+
+path1 <- "D:/BCI_Spatial/Lidar_Data/"
+path2 <- "D:/BCI_Spatial/UAV_Data/TiledPointClouds/"
+
+# 2017
 for(i in 1:dim(gridInfo)[1]){
-x1 <- gridInfo[i,"xmin"]
-x2 <- gridInfo[i,"xmax"]
-y1 <- gridInfo[i,"ymin"]
-y2 <- gridInfo[i,"ymax"]
-
-cropi <- raster::crop(dHeight18to19,
-                      raster::extent(c(x1,x2,y1,y2)))
-gridInfo$QC[i] <- mean(abs(cropi@data@values),na.rm=T)
+  
+  data <- lidR::clip_rectangle(las = lidR::readLAS(paste0(path2,"BCI17Tiles_aligned/BCI17a_",gridInfo$ID[i],".las")),
+                               xleft=gridInfo$xmin[i],
+                               xright=gridInfo$xmax[i],
+                               ybottom=gridInfo$ymin[i],
+                               ytop=gridInfo$ymax[i])
+  if(length(data@data$X)>0){
+    lidR::writeLAS(las = data,
+                   file = paste0(path2, "BCI17Tiles_alignedTrim/BCI17at_",gridInfo$ID[i],".laz"))
+  }
 }
 
-hist(gridInfo$QC)
+# Make canopy height raster for each year
 
-dim(gridInfo[gridInfo$QC >4,])
+cat17at <- lidR::catalog(paste0(path2,"BCI17Tiles_alignedTrim/"))
+
+chm15 <- raster::raster("CHM_2015_corrected.tif")
+
+chm17 <- lidR::grid_canopy(cat17at,
+                           res = 1,
+                           algorithm = lidR::p2r(subcircle=0.1))
+
+# Plot canopy height rasters and canopy height change
+colBrks1 <- seq(1,250)
+raster::plot(chm15,
+             main="Canopy height 2015",
+             col = terrain.colors(length(colBrks1), rev=T),
+             breaks = colBrks1)
+raster::plot(chm17,
+             main="Canopy height 2017",
+             col = terrain.colors(length(colBrks1), rev=T),
+             breaks = colBrks1)
+
+dHeight15to17 <- chm17-chm15
+
+colBrks2 <- c(-100,-20,-10,-5,-1,-0.5,0.5,1,5,10,20,100)
+colPal2 <- colorRampPalette(c("red","darksalmon","yellow",
+                              "white",
+                              "aliceblue","cornflowerblue","darkblue"))
+
+raster::plot(dHeight15to17,
+             col = colPal2(length(colBrks2)-1),
+             breaks = colBrks2,
+             main = "Corrected 2015 to 2017 height change")
+
+raster::writeRaster(chm17, file = "CHM_2017_corrected.tif")
+
+#### QAQC #####
+
+# Look for "holes" in each year
+soils <- rgdal::readOGR("D:/BCI_Spatial/BCI_Soils/BCI_Soils.shp")
+soils <- sp::spTransform(soils,"+proj=utm +zone=17 +datum=WGS84 +units=m +no_defs")
+
+# 2017
+gridInfo$QAQC17 <- NA
+
+chm17 <- lidR::grid_canopy(cat17at,
+                           res = 0.5,
+                           algorithm = lidR::p2r(subcircle=0.01))
+
+for(i in 1:dim(gridInfo)[1]){
+  x1 <- gridInfo[i, "xmin"] 
+  x2 <- gridInfo[i, "xmax"]
+  y1 <- gridInfo[i, "ymin"] 
+  y2 <- gridInfo[i, "ymax"] 
+  
+  cropi <- raster::crop(dHeight15to17,
+                        raster::extent(c(x1,x2,y1,y2)))
+  
+  gridInfo$QAQC1517[i] <-  length(cropi@data@values[is.na(cropi@data@values)])/length(cropi@data@values)
+}
+
+
+
+gridInfo$QAQC1517_mean <- NA
+gridInfo$QAQC1517_median <- NA
+
+for(i in 1:dim(gridInfo)[1]){
+ x1 <- gridInfo[i, "xmin"] 
+ x2 <- gridInfo[i, "xmax"]
+ y1 <- gridInfo[i, "ymin"] 
+ y2 <- gridInfo[i, "ymax"] 
+ 
+ cropi <- raster::crop(dHeight15to17,
+                       raster::extent(c(x1,x2,y1,y2)))
+ 
+ gridInfo$QAQC1517_mean[i] <- mean(abs(cropi@data@values), na.rm = T)
+ gridInfo$QAQC1517_median[i] <- median(abs(cropi@data@values), na.rm = T)
+}
+
+hist(gridInfo$QAQC1517_mean,
+     breaks = seq(0,100,0.5),
+     xlim=c(0,15),
+     col="black", border="white")
+hist(gridInfo$QAQC1517_median,
+     breaks = seq(0,100,0.5),
+     xlim=c(0,15),
+     col="black", border="white")
+
+head(gridInfo[gridInfo$QAQC1517_mean>4 & gridInfo$QAQC1517_mean<8,],100)
+
+gridInfo[gridInfo$xmin<628039.3  & gridInfo$xmax>628039.3  & gridInfo$ymin<1011579  & gridInfo$ymax>1011579 ,]
+
 
--- RetilePhotogrammetryData.R
+++ RetilePhotogrammetryData.R
@@ -1,14 +1,14 @@
-# Make lidR catalog entries for each dataset
+#### Make lidR catalog entries for each dataset ####
 cat09 <- lidR::catalog("D:/BCI_Spatial/Lidar_Data/lidar tiles 2009/lidar tiles 2009/")
   sp::proj4string(cat09) <- "+proj=utm +zone=17 +datum=WGS84 +units=m +no_defs"
 cat15 <- lidR::catalog("D:/BCI_Spatial/UAV_Data/OriginalPointClouds/2015_07_30_All_BCI_PointCloud.las")
-cat17 <- lidR::catalog("D:/BCI_Spatial/UAV_Data/OriginalPointClouds/2017_06_22_AllBCI_EBEE.las")
-cat18 <- lidR::catalog("D:/BCI_Spatial/UAV_Data/OriginalPointClouds/2018_Todos_BCI_Dipteryx_EBEE_PointCloud.las")
+cat17 <- lidR::catalog("C:/Users/cushmank/Desktop/2017_06_22_AllBCI_EBEE.las")
+cat18 <- lidR::catalog("C:/Users/cushmank/Desktop/2018_Todos_BCI_Dipteryx_EBEE_PointCloud.las")
 cat19 <- lidR::catalog("D:/BCI_Spatial/UAV_Data/OriginalPointClouds/2019_02_25_BCI_50ha.las")
 cat20 <- lidR::catalog("D:/BCI_Spatial/UAV_Data/OriginalPointClouds/2020_07_31_BCI_Dipteryx_Ebee_DensePts.las")
 
 
-# Create grid for new .las tiles
+#### Create grid for new .las tiles ####
   
   xmin <- 623400
   xmax <- 630100
@@ -31,6 +31,7 @@ cat20 <- lidR::catalog("D:/BCI_Spatial/UAV_Data/OriginalPointClouds/2020_07_31_B
   soils <- sp::spTransform(soils,"+proj=utm +zone=17 +datum=WGS84 +units=m +no_defs")
   
   gridInfo$Use <- NA
+  
   for(i in 1:dim(gridInfo)[1]){
     poly_i <- as(raster::extent(as.numeric(gridInfo[i,1:4])),
                      'SpatialPolygons')
@@ -54,9 +55,9 @@ cat20 <- lidR::catalog("D:/BCI_Spatial/UAV_Data/OriginalPointClouds/2020_07_31_B
   write.csv(gridInfo, row.names = F, file = "gridInfo.csv")
   
   
-overlap <- 25
+overlap <- 30
 
-# Retile 2009 lidar with overlap, and only keep highest points
+#### Retile 2009 lidar with overlap, and only keep highest points ####
   for(i in 1:dim(gridInfo)[1]){
     data <- lidR::lasclipRectangle(cat09, 
                                    xleft = gridInfo$xmin[i] - overlap,
@@ -66,13 +67,13 @@ overlap <- 25
     data <- lidR::decimate_points(data, algorithm = lidR::highest(res=2))
     
     if(length(data@data$X)>0){
-      lidR::writeLAS(data, file=paste0("D:/BCI Spatial Data/Lidar data/BCI09Tiles/BCI09_",i,".laz"))
+      lidR::writeLAS(data, file=paste0("D:/BCI_Spatial/Lidar_Data/BCI09Tiles/BCI09_",i,".laz"))
     }
     
     print(i)
   }
   
-  # Retile 2015 ppc with overlap
+#### Retile 2015 ppc with overlap ####
   for(i in 1:dim(gridInfo)[1]){
     data <- lidR::lasclipRectangle(cat15, 
                                    xleft = gridInfo$xmin[i] - overlap,
@@ -80,7 +81,38 @@ overlap <- 25
                                    xright = gridInfo$xmax[i] + overlap,
                                    ytop = gridInfo$ymax[i] + overlap)
     if(length(data@data$X)>0){
-      lidR::writeLAS(data, file=paste0("D:/BCI Spatial Data/UAV data/TiledPointClouds/BCI15Tiles/BCI15_",i,".laz"))
+      lidR::writeLAS(data, file=paste0("D:/BCI_Spatial/UAV_Data/TiledPointClouds/BCI15Tiles/BCI15_",i,".laz"))
+    }
+    
+    print(i)
+  }
+
+
+#### Retile 2017 ppc with overlap ####
+  for(i in 1:dim(gridInfo)[1]){
+    data <- lidR::lasclipRectangle(cat17, 
+                                   xleft = gridInfo$xmin[i] - overlap,
+                                   ybottom = gridInfo$ymin[i] - overlap,
+                                   xright = gridInfo$xmax[i] + overlap,
+                                   ytop = gridInfo$ymax[i] + overlap)
+    data <- lidR::decimate_points(data, algorithm = lidR::highest(res=0.5))
+    if(length(data@data$X)>0){
+      lidR::writeLAS(data, file=paste0("C:/Users/cushmank/Desktop/BCI17Tiles_dec/BCI17_",i,".laz"))
+    }
+    
+    print(i)
+  }
+
+#### Retile 2018 ppc with overlap ####
+  for(i in 1:dim(gridInfo)[1]){
+    data <- lidR::lasclipRectangle(cat18, 
+                                   xleft = gridInfo$xmin[i] - overlap,
+                                   ybottom = gridInfo$ymin[i] - overlap,
+                                   xright = gridInfo$xmax[i] + overlap,
+                                   ytop = gridInfo$ymax[i] + overlap)
+    data <- lidR::decimate_points(data, algorithm = lidR::highest(res=0.5))
+    if(length(data@data$X)>0){
+      lidR::writeLAS(data, file=paste0("C:/Users/cushmank/Desktop/BCI18Tiles_dec/BCI18_",i,".laz"))
     }
     
     print(i)
